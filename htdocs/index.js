589dcbd7d4dfb8a37a459f500732f3d43ed752b1
sv.junic@gmail.com <sv.junic@gmail.com>
2018-02-08 12:11:20 +0900

    てすと

---
+++ htdocs/index.js
@@ -0,0 +1,43 @@
+$(function(){
+
+  $('.toggle-hook').on('click', function(e){
+    e.preventDefault();
+    var $more = $(this).next('.toggle-more');
+    $more.css('height','auto');
+
+    var height = $more.height();
+
+    // スピード100のときのduration
+    var duration = height/10;
+    if( duration < 100 ){
+      duration = 100;
+    } else if( duration > 300 ) {
+      duration = 300;
+    }
+
+    $more.velocity('stop');
+    if( !$more.hasClass('open') ) {
+      $more.css('height','0');
+      $more.velocity({
+        height: height
+      }, {
+        easing  : 'swing',
+        duration: duration,
+        complete: function(){
+          console.log('open complete');
+        }
+      });
+    } else {
+      $more.velocity({
+        height: 0
+      }, {
+        easing  : 'swing',
+        duration: duration,
+        complete: function(){
+          console.log('close complete');
+        }
+      });
+    }
+    $more.toggleClass('open');
+  });
+});
